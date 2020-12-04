0281618a8fba7da42e194d78cf0dd4ea6d520e66
Callum <123callumb@gmail.com>
2020-12-04 19:08:23 +0000

    was a dumb idea to use reduce here but i might stick with it for a challenge idk this is taking while to figure out

--- src/fpassignment/question_4.clj
+++ src/fpassignment/question_4.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 ; This is so the months can be split into an array
-; e.g. (:monthTemp 5) for may temp. Should be useful
+; e.g. (:monthTemp 4) for may temp. Should be useful
 ; when iterating through the month calculations.
 ; Data structure looks like [int int int[]]
 (defrecord CetRecord [year day monthTemp])
@@ -18,9 +18,12 @@
              ; Trim the whitespaces infront and at the end of the lines.
              trimedLines (map #(str/trim %) lines)
              ; Seperate into a vecotor based on the spaces
-             formattedData (map #(str/split % #"\s+") trimedLines)
+             splitData (vec (map #(str/split % #"\s+") trimedLines))
+             ; Format data into integers, had to use double predicate here, It's ugly but i really didnt want to
+             ; do it outside of this.
+             formattedData  (map (fn [line] (vec (map #(Integer/parseInt %) line))) splitData)
              ; Place into a record so it is more readable
-             asRecords (map #(apply ->CetRecord [(% 0) (% 1) (subvec % 2)]) formattedData)]
+             asRecords (vec (map #(apply ->CetRecord [(% 0) (% 1) (subvec % 2)]) formattedData))]
          asRecords)
        (catch Exception ex (println "Exception: " (.toString ex)))
        (finally nil)))
@@ -28,5 +31,27 @@
 ; === Question 1 ===
 ; Warmest day for each calendar month
 ; loop through each record
+; result data struct should look somehting like:
+; { date: int, temp: int, year: int }[12]
+; The year isnt really needed that much here but it's
+; nice to see.
+(defrecord WarmestMonthDate [date temp year])
+
 (defn find-warmest-days []
-  (let [cet (get-cet)]))
+  (let [cet (get-cet)]
+    ; Null check just incase, can't hurt
+    (if (= cet nil)
+      (println "Something went wrong with reading the Cet data file :(")
+      ; warmest-months: This is the initial value, the index of the array represents the month,
+      ; e.g. (warmest-months 5) would be june.
+      ; I will be updating the date and year value as it goes along just to see which year and
+      ; date was the warmest.
+      ; Index: is the position in the cet data set we're looking at
+      (let [warmest-months (vec (repeat 12 (WarmestMonthDate. 0 -999 0)))
+            result (reduce (fn [dateRecord warmestMonths]
+                             (vec (map (fn [[monthIndex warmestVal]]
+                                         (let [currentTemp ((:monthTemp dateRecord) monthIndex)
+                                               currentWarmestTemp (:temp warmestVal)]
+                                           (if (> currentTemp currentWarmestTemp)
+                                             (WarmestMonthDate. (:day dateRecord) currentTemp (:year dateRecord))))) warmestMonths))) warmest-months cet)]
+        result))))
